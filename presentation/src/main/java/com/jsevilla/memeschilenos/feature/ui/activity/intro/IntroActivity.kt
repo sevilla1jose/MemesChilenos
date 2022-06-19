dbdc032aa44c5225f5ee7eedaf6e2c5d73495ae9
josesevilla <jose1.sevilla23@gmail.com>
2022-06-19 17:26:00 -0500

    Ending the introduction camera permission location permission

---
+++ presentation/src/main/java/com/jsevilla/memeschilenos/feature/ui/activity/intro/IntroActivity.kt
@@ -0,0 +1,67 @@
+package com.jsevilla.memeschilenos.feature.ui.activity.intro
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroCustomLayoutFragment.Companion.newInstance
+import com.github.appintro.AppIntroPageTransformerType
+import com.jsevilla.memeschilenos.R
+import com.jsevilla.memeschilenos.feature.ui.activity.home.HomeActivity
+
+class IntroActivity : AppIntro2() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        addSlide(newInstance(R.layout.intro_custom_layout1))
+        addSlide(newInstance(R.layout.intro_custom_layout2))
+        addSlide(newInstance(R.layout.intro_custom_layout3))
+        addSlide(newInstance(R.layout.intro_custom_layout4))
+
+        isIndicatorEnabled = true
+        isSystemBackButtonLocked = false
+        isButtonsEnabled = true
+        isSkipButtonEnabled = false
+
+        setTransformer(AppIntroPageTransformerType.SlideOver)
+        showStatusBar(show = true)
+
+        askForPermissions(
+            permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            slideNumber = 2,
+            required = true
+        )
+
+        askForPermissions(
+            permissions = arrayOf(
+                Manifest.permission.CAMERA
+            ),
+            slideNumber = 3,
+            required = true
+        )
+    }
+
+    public override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        finish()
+    }
+
+    public override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        val intent = Intent(this@IntroActivity, HomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+    }
+
+    override fun onUserDeniedPermission(permissionName: String) {
+        // User pressed "Deny" on the permission dialog
+    }
+
+    override fun onUserDisabledPermission(permissionName: String) {
+        // User pressed "Deny" + "Don't ask again" on the permission dialog
+    }
+}
